af41a1fec629e5e7c99e0398c460a0579defa001
Andrew da Silva <andrewairamdasilva@gmail.com>
2024-08-31 22:51:43 -0300

    Initial commit

---
+++ src/test/java/org/generation/school_app/SchoolAppApplicationTests.java
@@ -0,0 +1,8 @@
+package org.generation.school_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAppApplicationTests {
+}
