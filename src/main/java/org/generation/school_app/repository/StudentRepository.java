99ff1703279941b5980b9650d1b644245e7f4226
Andrew da Silva <andrewairamdasilva@gmail.com>
2024-08-31 22:55:09 -0300

    feat: student repository

---
+++ src/main/java/org/generation/school_app/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package org.generation.school_app.repository;
+
+import org.generation.school_app.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
